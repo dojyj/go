2dffd0b90e49a8eab42cb4afedc993110885ab5e
dojyj <dojyj@naver.com>
2021-12-27 21:21:02 +0900

    brute force

--- ps/14501.go
+++ ps/14501.go
@@ -29,38 +29,52 @@ func main() {
 
 		fmt.Scanf("%d %d\n", &T, &P)
 
+		// You can't choice scehdule after quit your job. But all schedule's time ticking
 		if time+T > N {
+			for i := range schedules {
+				if schedules[i].time > 0 {
+					schedules[i].time--
+				}
+			}
 			continue
 		}
 
+		// If this Schedule cannot be appended in any schedule
 		isAppended := false
 
+		// Iterate all Schedules and Modify or Create Schedule
 		for i := range schedules {
 			if schedules[i].time > 0 {
 				schedules[i].time--
 			}
+			// fmt.Println(schedules[i], P, T)
+			if schedules[i].time == 0 {
+				// Copy current pay array
+				pay_cpy := make([]int, len(schedules[i].pay))
+				copy(pay_cpy, schedules[i].pay)
 
-			if schedules[i].time <= 0 {
-				cpy := make([]int, len(schedules[i].pay))
-				copy(cpy, schedules[i].pay)
+				// case 1. Not append this schedule 
+				schedules = append(schedules, Schedule{time: schedules[i].time, pay: pay_cpy})
 
-				schedules = append(schedules, Schedule{time: schedules[i].time, pay: cpy}) // 추가하지 않는 경우
-				schedules[i].pay = append(schedules[i].pay, P)                             // 추가하는 경우
+				// case 2. Append this schedule
+				schedules[i].pay = append(schedules[i].pay, P)                             
 				schedules[i].time += T
+
+				// This schedule is appended to exists schedule. So it is no need to be created as new schedule
 				isAppended = true
 			} else {
 				continue
 			}
 		}
-
+		
 		if !isAppended {
 			schedules = append(schedules, Schedule{time: T})
 			last_idx := len(schedules) - 1
 			schedules[last_idx].pay = append(schedules[last_idx].pay, P)
 		}
-		// fmt.Println(schedules)
 	}
-
+	
+	// Get bset schedule that gives biggest money 
 	ans := 0
 	for _, schedule := range schedules {
 		ret := SumOfArray(schedule.pay)
